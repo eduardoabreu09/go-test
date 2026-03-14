3127c633b02fcbed7ee1eaae408fcae04a5ccbc5
Eduardo Abreu <eduardoabreu09@gmail.com>
2026-03-14 17:08:37 -0300

    Add farm starter code for api

---
+++ farm/cmd/api.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID) // important for rate limiting
+	r.Use(middleware.RealIP)    // analytics and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) // recover from crashes
+
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	// http.ListenAndServe(":3333", r)
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	server := &http.Server{
+		Addr: app.config.addr,
+		Handler: h,
+		WriteTimeout: time.Minute,
+		ReadTimeout: time.Second * 10,
+		IdleTimeout: time.Minute,
+	}
+
+	log.Printf("server has started at %s", app.config.addr)
+
+	return server.ListenAndServe()
+}
+
+type config struct {
+	addr string // URL:PORT
+	db dbConfig
+}
+
+type dbConfig struct {
+	connectionString string
+}
+
