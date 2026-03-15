f51659b78ccd9fae072d6d4df6bea7acaf87dcb6
Eduardo Abreu <eduardoabreu09@gmail.com>
2026-03-15 16:20:51 -0300

    Add env, db connection string and context

--- farm/cmd/main.go
+++ farm/cmd/main.go
@@ -1,24 +1,40 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/eduardoabreu09/farm/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg := config{
 		addr: ":8080",
 		db: dbConfig{
-			connectionString: "host=localhost user=postgres password=admin dbname=farm sslmode=disabled",
+			connectionString: env.GetString("DATABASE_URL", "host=localhost user=postgres password=admin dbname=farm sslmode=disable"),
 		},
-	}
-	api := application{
-		config: cfg,
 	}
 
 	// logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	// Database
+	conn, err := pgx.Connect(ctx, cfg.db.connectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connect to database", "connectionString", cfg.db.connectionString)
+
+	api := application{
+		config: cfg,
+	}
 
 	if err := api.run(api.mount()); err != nil {
 		slog.Error("server has failed", "error", err)
